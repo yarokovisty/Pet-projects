abf4d3554862cbb3c25e39bdceb5f8842176f951
yarokovisty <yarokovisty@gmail.com>
2026-02-03 17:06:44 +0700

    feat: init delivery project

---
+++ Delivery/composeApp/src/commonMain/kotlin/org/yarokovisty/delivery/App.kt
@@ -0,0 +1,27 @@
+package org.yarokovisty.delivery
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.resources.painterResource
+
+import delivery.composeapp.generated.resources.Res
+import delivery.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+fun App() {
+    MaterialTheme {
+
+    }
+}
---
+++ Delivery/composeApp/src/iosMain/kotlin/org/yarokovisty/delivery/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.yarokovisty.delivery
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
