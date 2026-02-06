c9ae39c602b53bb07baeaceb4c00607a0a5fdcf5
yarokovisty <yarokovisty@gmail.com>
2026-02-06 13:57:36 +0700

    feat(di): add Koin dependency injection configuration

---
+++ Delivery/composeApp/src/commonMain/kotlin/org/yarokovisty/delivery/di/Koin.kt
@@ -0,0 +1,8 @@
+package org.yarokovisty.delivery.di
+
+import org.koin.dsl.KoinConfiguration
+import org.koin.dsl.koinConfiguration
+
+fun getKoinConfiguration(): KoinConfiguration =
+    koinConfiguration {
+    }
